2bef8a3fdd67ff5033bd29dbd7957653f3006232
Esti Gajda <egajda@umich.edu>
2018-09-05 14:33:29 -0400

    Make nav mobile responsive

--- config.rb
+++ config.rb
@@ -9,10 +9,6 @@ activate :gh_pages do |gh_pages|
   gh_pages.remote = 'git@github.com:maavumich/maavumich.github.io.git'
   gh_pages.branch = 'master'
 end
-# Layouts
-# https://middlemanapp.com/basics/layouts/
-
-# Per-page layout changes
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
@@ -26,35 +22,3 @@ set :images_dir, 'images'
 
 activate :minify_css
 activate :relative_assets
-
-# With alternative layout
-# page '/path/to/file.html', layout: 'other_layout'
-
-# Proxy pages
-# https://middlemanapp.com/advanced/dynamic-pages/
-
-# proxy(
-#   '/this-page-has-no-template.html',
-#   '/template-file.html',
-#   locals: {
-#     which_fake_page: 'Rendering a fake page with a local variable'
-#   },
-# )
-
-# Helpers
-# Methods defined in the helpers block are available in templates
-# https://middlemanapp.com/basics/helper-methods/
-
-# helpers do
-#   def some_helper
-#     'Helping'
-#   end
-# end
-
-# Build-specific configuration
-# https://middlemanapp.com/advanced/configuration/#environment-specific-settings
-
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
-# end
