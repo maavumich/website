c7a91602a0a5042abfa0fa8fc0b0330f95255339
Esti Gajda <egajda@umich.edu>
2018-09-05 13:28:30 -0400

    Update README

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # MAAV Website
+
+## Contributing
+- Make sure you have ruby installed
+- Clone this repo
+- `bundle install`
+- To view work locally run `middleman`
+- Deploy changes with `middleman gh_pages`
--- config.rb
+++ config.rb
@@ -5,7 +5,10 @@ activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
 activate :directory_indexes
-
+activate :gh_pages do |gh_pages|
+  gh_pages.remote = 'git@github.com:maavumich/maavumich.github.io.git'
+  gh_pages.branch = 'master'
+end
 # Layouts
 # https://middlemanapp.com/basics/layouts/
 
@@ -18,6 +21,12 @@ set :haml, format: :html5
 page 'index', layout: 'layout'
 page 'about', layout: 'layout'
 page 'join/index', layout: false
+set :css_dir, 'stylesheets'
+set :images_dir, 'images'
+
+activate :minify_css
+activate :relative_assets
+
 # With alternative layout
 # page '/path/to/file.html', layout: 'other_layout'
 
